e3cfd7d1cfcca7d89bbb9c4ecd9dbc2a39cfc6da
Louis <coleloui18@gmail.com>
2024-01-23 12:20:59 -0800

    dropping tables

--- basic.sql
+++ basic.sql
@@ -1,12 +1,16 @@
-USE pet_shop;
+-- USE pet_shop;
 
-CREATE TABLE cats (
-    name VARCHAR(50),
-    age INT
-);
+-- CREATE TABLE cats (
+--     name VARCHAR(50),
+--     age INT
+-- );
 
-CREATE TABLE dogs (
-    name VARCHAR(50),
-    breed VARCHAR(50),
-    age INT
-);
+-- CREATE TABLE dogs (
+--     name VARCHAR(50),
+--     breed VARCHAR(50),
+--     age INT
+-- );
+
+-- DESC cats;
+
+DROP TABLE cats;
