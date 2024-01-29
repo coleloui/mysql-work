68fe2d3f61f6bc05f7a59cf80172d1701bad5fef
Louis <coleloui18@gmail.com>
2024-01-29 10:56:32 -0800

    table exercise

--- basic.sql
+++ basic.sql
@@ -24,15 +24,30 @@
 
 -- DROP TABLE pastries;
 
-CREATE TABLE cats(
-    name VARCHAR(50),
-    age INT
-);
+-- CREATE TABLE cats(
+--     name VARCHAR(50),
+--     age INT
+-- );
 
 -- INSERT INTO cats (name, age) VALUES ("Niko", 6);
 -- INSERT INTO cats (name, age) VALUES ("Bandit", 6);
 -- INSERT INTO cats (name, age) VALUES ("Newt", 1);
 
-INSERT INTO cats (name, age) VALUES ("October", 2), ("June", 7), ("Meatball", 3);
+-- INSERT INTO cats (name, age) VALUES ("October", 2), ("June", 7), ("Meatball", 3);
 
-SELECT * FROM cats;
+-- SELECT * FROM cats;
+
+CREATE TABLE people (
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    age INT
+);
+
+INSERT INTO people (first_name, last_name, age) VALUES ("Tina", "Blecher", 13);
+INSERT INTO people (first_name, last_name, age) VALUES ("Bob", "Blecher", 42);
+
+INSERT INTO people (first_name, last_name, age) VALUES ("Linda", "Blecher", 45), ("Phillip", "Frond", 38), ("Calvin", "Fischoeder", 70);
+
+SELECT * FROM people;
+
+DROP TABLE people;
