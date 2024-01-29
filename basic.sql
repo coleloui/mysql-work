6757ece478afc5bd3087a710ee32976ff9b1e7e9
Louis <coleloui18@gmail.com>
2024-01-29 09:42:33 -0800

    tables activity pastries

--- basic.sql
+++ basic.sql
@@ -13,4 +13,13 @@
 
 -- DESC cats;
 
-DROP TABLE cats;
+-- USE pet_shop;
+
+-- CREATE TABLE pastries (
+--     name VARCHAR(50),
+--     quantity INT
+-- );
+
+-- DESC pastries;
+
+-- DROP TABLE pastries;
