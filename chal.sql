c980217d7a72f95d6e4d0c5e55f40a412df5447f
Louis <coleloui18@gmail.com>
2024-01-30 12:32:14 -0800

    delete exercise

--- chal.sql
+++ chal.sql
@@ -24,10 +24,16 @@
 -- SELECT cat_id, age FROM cats WHERE age = cat_id;
 -- SELECT name, age FROM cats WHERE age = cat_id;
 
-UPDATE cats SET name = 'Jack' WHERE name = 'Jackson';
+-- UPDATE cats SET name = 'Jack' WHERE name = 'Jackson';
 
-UPDATE cats SET breed = 'Brittish Shorthair' WHERE name = 'Ringo';
+-- UPDATE cats SET breed = 'Brittish Shorthair' WHERE name = 'Ringo';
 
-UPDATE cats SET age = 12 WHERE breed = 'Maine Coon';
+-- UPDATE cats SET age = 12 WHERE breed = 'Maine Coon';
+
+DELETE FROM cats WHERE age = 4;
+
+DELETE FROM cats WHERE age = cat_id;
+
+DELETE FROM cats;
 
 SELECT * FROM cats;
--- crud.sql
+++ crud.sql
@@ -27,4 +27,6 @@ USE pet_shop;
 -- UPDATE cats SET breed = 'Shorthair' WHERE breed = 'Tabby';
 UPDATE cats SET age = 14 WHERE name = 'Misty';
 
+DELETE FROM cats WHERE name = 'Egg';
+
 SELECT name, age FROM cats;
