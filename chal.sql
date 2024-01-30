e4e1c2b2ce38f229be6041de75a116512791e0f0
Louis <coleloui18@gmail.com>
2024-01-30 11:17:32 -0800

    where and challange

--- chal.sql
+++ chal.sql
@@ -1,15 +1,23 @@
-USE chicken_coop;
+-- USE chicken_coop;
 
-CREATE TABLE employees(
-    id INT AUTO_INCREMENT,
-    first_name VARCHAR(20) NOT NULL,
-    middle_name VARCHAR(20),
-    last_name VARCHAR(20) NOT NULL,
-    age INT NOT NULL,
-    current_status VARCHAR(20) NOT NULL DEFAULT 'employeed',
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE employees(
+--     id INT AUTO_INCREMENT,
+--     first_name VARCHAR(20) NOT NULL,
+--     middle_name VARCHAR(20),
+--     last_name VARCHAR(20) NOT NULL,
+--     age INT NOT NULL,
+--     current_status VARCHAR(20) NOT NULL DEFAULT 'employeed',
+--     PRIMARY KEY(id)
+-- );
 
-DESC employees;
+-- DESC employees;
 
-DROP TABLE employees;
+-- DROP TABLE employees;
+
+-- USE pet_shop;
+
+-- SELECT cat_id FROM cats;
+-- SELECT name, breed FROM cats;
+-- SELECT name, age FROM cats WHERE breed = "Tabby";
+-- SELECT cat_id, age FROM cats WHERE age = cat_id;
+-- SELECT name, age FROM cats WHERE age = cat_id;
