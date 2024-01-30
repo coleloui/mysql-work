68cd8fafc8c0725c5da2af516e7970ddbddfc4f5
Louis <coleloui18@gmail.com>
2024-01-30 11:42:39 -0800

    updating

--- chal.sql
+++ chal.sql
@@ -1,16 +1,18 @@
 -- USE chicken_coop;
 
--- CREATE TABLE employees(
---     id INT AUTO_INCREMENT,
---     first_name VARCHAR(20) NOT NULL,
---     middle_name VARCHAR(20),
---     last_name VARCHAR(20) NOT NULL,
---     age INT NOT NULL,
---     current_status VARCHAR(20) NOT NULL DEFAULT 'employeed',
---     PRIMARY KEY(id)
+-- CREATE TABLE employees (
+--     id INT AUTO_INCREMENT, first_name VARCHAR(20) NOT NULL, middle_name VARCHAR(20), last_name VARCHAR(20) NOT NULL, age INT NOT NULL, current_status VARCHAR(20) NOT NULL DEFAULT 'employeed', PRIMARY KEY (id)
 -- );
 
 -- DESC employees;
+
+-- UPDATE employees
+-- SET
+--     current_status = 'laid-off'
+-- WHERE
+--     current_status = 'employed';
+
+-- SELECT * FROM employees;
 
 -- DROP TABLE employees;
 
--- crud.sql
+++ crud.sql
@@ -1,23 +1,30 @@
 USE pet_shop;
 
-CREATE TABLE cats (
-    cat_id INT AUTO_INCREMENT,
-    name VARCHAR(100),
-    breed VARCHAR(100),
-    age INT,
-    PRIMARY KEY (cat_id)
-); 
+-- CREATE TABLE cats (
+--     cat_id INT AUTO_INCREMENT, name VARCHAR(100), breed VARCHAR(100), age INT, PRIMARY KEY (cat_id)
+-- );
 
-INSERT INTO cats(name, breed, age) 
-VALUES ('Ringo', 'Tabby', 4),
-       ('Cindy', 'Maine Coon', 10),
-       ('Dumbledore', 'Maine Coon', 11),
-       ('Egg', 'Persian', 4),
-       ('Misty', 'Tabby', 13),
-       ('George Michael', 'Ragdoll', 9),
-       ('Jackson', 'Sphynx', 7);
+-- INSERT INTO
+--     cats (name, breed, age)
+-- VALUES ('Ringo', 'Tabby', 4),
+--     ('Cindy', 'Maine Coon', 10),
+--     (
+--         'Dumbledore', 'Maine Coon', 11
+--     ),
+--     ('Egg', 'Persian', 4),
+--     ('Misty', 'Tabby', 13),
+--     (
+--         'George Michael', 'Ragdoll', 9
+--     ),
+--     ('Jackson', 'Sphynx', 7);
 
 -- SELECT name, breed FROM cats;
 -- SELECT name, age FROM cats;
 
-SELECT name, age FROM cats WHERE age = 4;
+-- SELECT name, age FROM cats WHERE age = 4;
+-- SELECT cat_id as ID FROM cats;
+
+-- UPDATE cats SET breed = 'Shorthair' WHERE breed = 'Tabby';
+UPDATE cats SET age = 14 WHERE name = 'Misty';
+
+SELECT name, age FROM cats;
