44b50be4f24d1f02d3955c1356e180501a53efdd
Louis <coleloui18@gmail.com>
2024-01-30 12:19:37 -0800

    exercise update

--- chal.sql
+++ chal.sql
@@ -23,3 +23,11 @@
 -- SELECT name, age FROM cats WHERE breed = "Tabby";
 -- SELECT cat_id, age FROM cats WHERE age = cat_id;
 -- SELECT name, age FROM cats WHERE age = cat_id;
+
+UPDATE cats SET name = 'Jack' WHERE name = 'Jackson';
+
+UPDATE cats SET breed = 'Brittish Shorthair' WHERE name = 'Ringo';
+
+UPDATE cats SET age = 12 WHERE breed = 'Maine Coon';
+
+SELECT * FROM cats;
