083c0bbb22711a3e270574113e118d42b17139ef
Louis <coleloui18@gmail.com>
2024-01-30 12:56:54 -0800

    mega challange shirts

--- chal.sql
+++ chal.sql
@@ -30,10 +30,10 @@
 
 -- UPDATE cats SET age = 12 WHERE breed = 'Maine Coon';
 
-DELETE FROM cats WHERE age = 4;
+-- DELETE FROM cats WHERE age = 4;
 
-DELETE FROM cats WHERE age = cat_id;
+-- DELETE FROM cats WHERE age = cat_id;
 
-DELETE FROM cats;
+-- DELETE FROM cats;
 
-SELECT * FROM cats;
+-- SELECT * FROM cats;
---
+++ shirts.sql
@@ -0,0 +1,73 @@
+CREATE DATABASE shirts;
+
+USE shirts;
+
+CREATE TABLE shirts (
+    shirt_id INT AUTO_INCREMENT PRIMARY KEY, article VARCHAR(20) NOT NULL, color VARCHAR(20) NOT NULL, shirt_size VARCHAR(4) NOT NULL, last_worn INT NOT NULL DEFAULT 0
+);
+
+INSERT INTO
+    shirts (
+        article, color, shirt_size, last_worn
+    )
+VALUES ('t-shirt', 'white', 'S', 10),
+    ('t-shirt', 'green', 'S', 200),
+    (
+        'polo shirt', 'black', 'M', 10
+    ),
+    ('tank top', 'blue', 'S', 50),
+    ('t-shirt', 'pink', 'S', 0),
+    ('polo shirt', 'red', 'M', 5),
+    ('tank top', 'white', 'S', 200),
+    ('tank top', 'blue', 'M', 15);
+
+INSERT INTO
+    shirts (
+        article, color, shirt_size, last_worn
+    )
+VALUES (
+        'polo shirt', 'purple', 'M', 50
+    );
+
+SELECT * FROM shirts WHERE color = 'black';
+
+SELECT
+    article,
+    color,
+    shirt_size,
+    last_worn
+FROM shirts
+WHERE
+    shirt_size = 'M';
+
+UPDATE shirts
+SET
+    shirt_size = 'L'
+WHERE
+    article = 'polo shirt';
+
+SELECT
+    article,
+    color,
+    shirt_size,
+    last_worn
+FROM shirts
+WHERE
+    shirt_size = 'L';
+
+UPDATE shirts SET last_worn = 0 WHERE last_worn = 15;
+
+UPDATE shirts
+SET
+    shirt_size = 'XS',
+    color = 'off white'
+WHERE
+    color = 'white';
+
+DELETE FROM shirts WHERE last_worn >= 200;
+
+DELETE FROM shirts where article = 'tank top';
+
+DELETE FROM shirts;
+
+DROP TABLE shirts;
