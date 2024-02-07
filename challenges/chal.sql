5d108ff7b29c2a621e47c9896413640f419353d9
Louis <coleloui18@gmail.com>
2024-02-07 11:45:12 -0800

    string challenges

--- challenges/chal.sql
+++ challenges/chal.sql
@@ -37,3 +37,39 @@
 -- DELETE FROM cats;
 
 -- SELECT * FROM cats;
+
+SELECT UPPER(
+        REVERSE(
+            'Why does my cat look at me with such hatred?'
+        )
+    );
+
+SELECT REPLACE (title, ' ', '->') AS title FROM books;
+
+SELECT
+    author_fname AS forwards,
+    REVERSE(author_fname) AS backwards
+FROM books;
+
+SELECT UPPER(
+        CONCAT(
+            author_fname, ' ', author_lname
+        )
+    ) AS 'full name in caps'
+FROM books;
+
+SELECT CONCAT(
+        title, ' was released in ', released_year
+    ) as blurb
+from books;
+
+SELECT title, CHARACTER_LENGTH(title) as 'character count'
+from books;
+
+SELECT
+    CONCAT(SUBSTR(title, 1, 10), '...') AS short_title,
+    CONCAT(
+        author_lname, ', ', author_fname
+    ) as author,
+    CONCAT(stock_quantity, ' in stock') as quantity
+FROM books;
