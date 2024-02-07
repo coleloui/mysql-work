0f52cb725303bb614766e60a05f406dc326bc5de
Louis <coleloui18@gmail.com>
2024-02-07 10:24:49 -0800

    concat substring

--- stringfnc/strings.sql
+++ stringfnc/strings.sql
@@ -14,4 +14,9 @@ FROM books;
 -- substring
 SELECT SUBSTRING('Hello World', 1, 5);
 
-SELECT CONCAT(SUBSTR(title, 1, 10), '...') FROM books;
+SELECT CONCAT(SUBSTR(title, 1, 10), '...') AS short_title FROM books;
+
+SELECT CONCAT(
+        SUBSTR(author_fname, 1, 1), '. ', SUBSTR(author_lname, 1, 1), '.'
+    ) AS author_initials
+FROM books;
