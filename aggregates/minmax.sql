fbb61831b25b994a9a357d0919513d47c930ed2c
Louis Coleman <coleloui18@gmail.com>
2024-09-17 11:04:12 -0700

    group by and min max

--- aggregates/minmax.sql
+++ aggregates/minmax.sql
@@ -24,3 +24,31 @@ WHERE
         SELECT MIN(released_year)
         FROM books
     );
+
+-- group by with min max
+SELECT author_fname, author_lname, MIN(released_year)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
+
+SELECT author_fname, author_lname, MAX(released_year)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
+
+SELECT
+    CONCAT(
+        SUBSTR(author_fname, 1, 1),
+        '. ',
+        author_lname
+    ) AS author,
+    COUNT(*) AS books_written,
+    MIN(released_year) AS earliest_release,
+    MAX(released_year) AS latest_release,
+    MAX(pages) AS longest_book
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
