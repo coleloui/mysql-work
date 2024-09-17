2f58b63421fc13ee5933a6c7ef4f07c8ca7b7597
Louis Coleman <coleloui18@gmail.com>
2024-09-17 10:12:26 -0700

    min man subqueries

--- aggregates/minmax.sql
+++ aggregates/minmax.sql
@@ -3,3 +3,24 @@ SELECT MIN(released_year) FROM books;
 SELECT MAX(pages) FROM books;
 
 SELECT MIN(author_lname) FROM books;
+
+-- subqueries
+SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
+
+SELECT * FROM books WHERE pages = ( SELECT MIN(pages) FROM books );
+
+SELECT title, pages
+FROM books
+WHERE
+    pages = (
+        SELECT MAX(pages)
+        FROM books
+    );
+
+SELECT title, released_year
+FROM books
+WHERE
+    released_year = (
+        SELECT MIN(released_year)
+        FROM books
+    );
