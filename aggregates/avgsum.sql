3c932da4c9ec31a31f36c55f37ec07f29e120633
Louis Coleman <coleloui18@gmail.com>
2024-09-18 08:22:43 -0700

    avg

--- aggregates/avgsum.sql
+++ aggregates/avgsum.sql
@@ -1,3 +1,4 @@
+-- sum
 SELECT SUM(pages) FROM books;
 
 SELECT author_lname, SUM(pages)
@@ -5,3 +6,15 @@ FROM books
 GROUP BY
     author_lname,
     author_fname;
+
+-- avg
+SELECT AVG(released_year) FROM books;
+
+SELECT AVG(pages) FROM books;
+
+SELECT AVG(stock_quantity) FROM books;
+
+SELECT released_year, AVG(stock_quantity)
+FROM books
+GROUP BY
+    released_year;
