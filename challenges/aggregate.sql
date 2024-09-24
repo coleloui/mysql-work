a9984c9b2524202db53d36a402beed146b434708
Louis Coleman <coleloui18@gmail.com>
2024-09-23 17:31:09 -0700

    challenges

--- challenges/aggregate.sql
+++ challenges/aggregate.sql
@@ -3,3 +3,24 @@ SELECT COUNT(*) FROM books;
 SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
 
 SELECT SUM(stock_quantity) FROM books;
+
+SELECT author_fname, author_lname, AVG(released_year)
+FROM books
+GROUP BY
+    author_lname,
+    author_fname;
+
+SELECT CONCAT(
+        author_fname, ' ', author_lname
+    ), pages
+FROM books
+WHERE
+    pages = (
+        SELECT MAX(pages)
+        FROM books
+    );
+
+SELECT released_year, COUNT(*) as '# books', AVG(pages) as 'avg pages'
+FROM books
+GROUP BY
+    released_year;
