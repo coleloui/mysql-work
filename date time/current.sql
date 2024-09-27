af14d0358c2ac012225f1a43920dd956ed4387bf
Louis Coleman <coleloui18@gmail.com>
2024-09-27 14:12:05 -0700

    date functions

--- date time/current.sql
+++ date time/current.sql
@@ -17,3 +17,9 @@ VALUES (
         CURTIME(),
         NOW()
     );
+
+SELECT birthdate, DAY(birthdate), DAYOFWEEK(birthdate), DAYOFYEAR(birthdate)
+FROM people;
+
+SELECT birthdate, MONTHNAME(birthdate), YEAR(birthdate)
+FROM people;
