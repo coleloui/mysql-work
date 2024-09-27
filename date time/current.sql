41eb2dfee0f39fea76caabf9bd7c0e23b954ca3f
Louis Coleman <coleloui18@gmail.com>
2024-09-27 13:04:56 -0700

    cur functions

--- date time/current.sql
+++ date time/current.sql
@@ -3,3 +3,17 @@ SELECT NOW();
 SELECT CURDATE();
 
 SELECT CURTIME();
+
+INSERT INTO
+    people (
+        name,
+        birthdate,
+        birthtime,
+        birthdt
+    )
+VALUES (
+        'Hazel',
+        CURDATE(),
+        CURTIME(),
+        NOW()
+    );
