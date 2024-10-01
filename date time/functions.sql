b5bbdfc5b7892c9c31a668d8e691757e607037cb
Louis Coleman <coleloui18@gmail.com>
2024-10-01 11:48:48 -0700

    time functions

---
+++ date time/functions.sql
@@ -0,0 +1,7 @@
+SELECT name, birthtime, HOUR(birthtime) FROM people;
+
+SELECT name, birthtime, MINUTE(birthtime) FROM people;
+
+SELECT name, birthtime, SECOND(birthtime) FROM people;
+
+SELECT name, birthtime, SECOND(birthdt) FROM people;
