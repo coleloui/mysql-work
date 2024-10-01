575e3e6525fafc8ed86fe7c86fd233e9891f867f
Louis Coleman <coleloui18@gmail.com>
2024-10-01 14:49:23 -0700

    date format

--- date time/functions.sql
+++ date time/functions.sql
@@ -5,3 +5,19 @@ SELECT name, birthtime, MINUTE(birthtime) FROM people;
 SELECT name, birthtime, SECOND(birthtime) FROM people;
 
 SELECT name, birthtime, SECOND(birthdt) FROM people;
+
+SELECT MONTHNAME(birthdate), DAY(birthdate), YEAR(birthdate)
+FROM people;
+
+SELECT CONCAT(
+        MONTHNAME(birthdate), ' ', DAY(birthdate), ' ', YEAR(birthdate)
+    )
+FROM people;
+
+SELECT DATE_FORMAT(birthdate, '%b') FROM people;
+
+SELECT DATE_FORMAT(birthdate, '%a %M %D %Y') FROM people;
+
+SELECT DATE_FORMAT(birthdt, '%h:%i') FROM people;
+
+SELECT DATE_FORMAT(birthdt, '%r') FROM people;
