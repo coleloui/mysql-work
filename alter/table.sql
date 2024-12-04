cfc19d3b3a85c8f797c29a38278114d69ccf4fb0
Louis Coleman <coleloui18@gmail.com>
2024-12-04 09:41:05 -0800

    alter table add column

---
+++ alter/table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE companies (
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    CONSTRAINT name_address UNIQUE (name, address)
+);
+
+ALTER Table companies ADD COLUMN phone VARCHAR(15);
+
+SELECT * FROM companies;
+
+ALTER Table companies
+ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
