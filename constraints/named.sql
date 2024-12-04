a6c5bb1030db59263aa1ed466f93823f0404c455
Louis Coleman <coleloui18@gmail.com>
2024-12-04 09:26:40 -0800

    named plaindrome2

--- constraints/named.sql
+++ constraints/named.sql
@@ -7,3 +7,8 @@ CREATE Table users (
 INSERT INTO users (username, age) VALUES ('technokid', 10);
 
 INSERT INTO users (username, age) VALUES ('imaginary', -1);
+
+CREATE TABLE palindromes2 (
+    word VARCHAR(100),
+    CONSTRAINT word_is_palindrome CHECK (REVERSE(word) = word)
+);
