66d4ddc547478951a169258b568179fe02ccb379
Louis Coleman <coleloui18@gmail.com>
2024-12-18 13:31:19 -0800

    inner join

--- join/inner_join.sql
+++ join/inner_join.sql
@@ -1,3 +1,9 @@
 SELECT *
 FROM customers
     JOIN orders ON customers.id = orders.customer_id;
+
+SELECT CONCAT(first_name, ' ', last_name), order_date, amount
+FROM customers
+    JOIN orders on orders.customer_id = customers.id;
+
+SELECT * FROM orders JOIN customers ON customers.id = orders.id;
