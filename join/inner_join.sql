f001f3b23d992e88b2d2edd428c63d8dc36229ca
Louis Coleman <coleloui18@gmail.com>
2024-12-18 13:36:37 -0800

    group and order by and join

--- join/inner_join.sql
+++ join/inner_join.sql
@@ -7,3 +7,10 @@ FROM customers
     JOIN orders on orders.customer_id = customers.id;
 
 SELECT * FROM orders JOIN customers ON customers.id = orders.id;
+
+SELECT CONCAT(first_name, ' ', last_name) AS customer, SUM(amount) as total
+FROM customers
+    JOIN orders on orders.customer_id = customers.id
+GROUP BY
+    customer
+ORDER BY total;
